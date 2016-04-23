1f52ac06eba48e34995318f2c47b9befb36e57a1
Vasily Tarasov <flame4288@gmail.com>
2016-04-23 12:39:17 +0600

    Add map intent

---
+++ app/src/androidTest/java/com/example/android/sunshine/app/ForecastFragmentTest.java
@@ -0,0 +1,10 @@
+package com.example.android.sunshine.app;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Flame on 15.04.2016.
+ */
+public class ForecastFragmentTest extends TestCase {
+
+}
